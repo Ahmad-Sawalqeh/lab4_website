96f4fdeffae2217f2b1b8b3efc61fad5a13c3e82
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-10-02 16:20:26 +0300

    adding js for asking the user using loop

--- js/javascsript.js
+++ js/javascsript.js
@@ -1,4 +1,5 @@
-/* eslint-disable strict */
+// /* eslint-disable strict */
+// eslint-disable-next-line strict
 var bg_color = document.getElementById("bg_color");
 var confirmMe = document.getElementById("confirmMe");
 var alertMe = document.getElementById("alertMe");
@@ -16,10 +17,13 @@ alertMe.addEventListener("click",function(){
     alert("Do NOT close our website");
 });
 
-var player = prompt("How many football player in one team?");
 
+var player = prompt("How many football player in one team?");
+// console.log("type of = "+typeof(player));
+
+while(player!=11){
+    player = prompt("Sorry try again, How many football player in one team?");
+}
 if(player == 11){
     alert("correct answer");
-}else{
-    alert("sorry try again!");
 }
