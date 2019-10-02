efc8a1d89e1dbebecfff9f39f9ee0fa3562d453f
ahmad-sawalqeh <sawalqa_jo@hotmail.com>
2019-10-02 16:30:43 +0300

    loop

--- js/javascsript.js
+++ js/javascsript.js
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 // /* eslint-disable strict */
 // eslint-disable-next-line strict
 var bg_color = document.getElementById("bg_color");
@@ -5,16 +6,16 @@ var confirmMe = document.getElementById("confirmMe");
 var alertMe = document.getElementById("alertMe");
 
 bg_color.addEventListener("click",function(){
-    var color = prompt("write the name of the new background-color:");
-    document.body.style.backgroundColor = color;
+  var color = prompt("write the name of the new background-color:");
+  document.body.style.backgroundColor = color;
 });
 
 confirmMe.addEventListener("click",function(){
-    confirm("Liverpool won the champion lats year");
+  confirm("Liverpool won the champion lats year");
 });
 
 alertMe.addEventListener("click",function(){
-    alert("Do NOT close our website");
+  alert("Do NOT close our website");
 });
 
 
@@ -22,8 +23,8 @@ var player = prompt("How many football player in one team?");
 // console.log("type of = "+typeof(player));
 
 while(player!=11){
-    player = prompt("Sorry try again, How many football player in one team?");
+  player = prompt('Sorry try again, How many football player in one team?');
 }
 if(player == 11){
-    alert("correct answer");
+  alert("correct answer");
 }
