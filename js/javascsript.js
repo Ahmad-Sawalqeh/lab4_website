c22c8665b6b3b8dc27e15e3fd0d77ed53fa6050c
Ahmad Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2019-10-03 11:11:44 +0300

    Update javascsript.js

--- js/javascsript.js
+++ js/javascsript.js
@@ -1,33 +1,33 @@
 // /* eslint-disable quotes */
 // // /* eslint-disable strict */
 // // eslint-disable-next-line strict
-// var bg_color = document.getElementById("bg_color");
-// var confirmMe = document.getElementById("confirmMe");
-// var alertMe = document.getElementById("alertMe");
+var bg_color = document.getElementById("bg_color");
+var confirmMe = document.getElementById("confirmMe");
+var alertMe = document.getElementById("alertMe");
 
-// bg_color.addEventListener("click",function(){
-//   var color = prompt("write the name of the new background-color:");
-//   document.body.style.backgroundColor = color;
-// });
+bg_color.addEventListener("click",function(){
+  var color = prompt("write the name of the new background-color:");
+  document.body.style.backgroundColor = color;
+});
 
-// confirmMe.addEventListener("click",function(){
-//   confirm("Liverpool won the champion lats year");
-// });
+confirmMe.addEventListener("click",function(){
+  confirm("Liverpool won the champion lats year");
+});
 
-// alertMe.addEventListener("click",function(){
-//   alert("Do NOT close our website");
-// });
+alertMe.addEventListener("click",function(){
+  alert("Do NOT close our website");
+});
 
 
-// var player = prompt("How many football player in one team?");
-// // console.log("type of = "+typeof(player));
+var player = prompt("How many football player in one team?");
+// console.log("type of = "+typeof(player));
 
-// while(player!=11){
-//   player = prompt('Sorry try again, How many football player in one team?');
-// }
-// if(player == 11){
-//   alert("correct answer");
-// }
+while(player!=11){
+  player = prompt('Sorry try again, How many football player in one team?');
+}
+if(player == 11){
+  alert("correct answer");
+}
 
 var showorder=function () {
   //input
